7e56464286469a35e3b3f26b1e4cf197ab21cd13
Al-Haadee <alhaadee91@gmail.com>
2023-04-23 19:24:29 +0100

    creates vite react app

---
+++ portfolio-website/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Layout from './components/Layout/Layout'
+import { Route, Routes } from 'react-router-dom' 
+
+function App() {
+  
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element = {<Layout />}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ portfolio-website/src/components/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import Sidebar from '../sidebar/Sidebar'
+import './Layout.css'
+
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <Sidebar/>
+  )
+}
+
+export default Layout
