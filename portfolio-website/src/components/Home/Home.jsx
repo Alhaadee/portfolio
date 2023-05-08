1d507c07338accf96b15a3a08dd121a36c6cca5b
Al-Haadee <alhaadee91@gmail.com>
2023-05-08 11:42:40 +0100

    adds fonts

--- portfolio-website/src/components/Home/Home.jsx
+++ portfolio-website/src/components/Home/Home.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './home.css'
 
 const Home = () => {
   return (
     <div className='container home-page'>
         <div className='text-zone'>
             <h1> Hi, <br /> I'm
-                <img src="" alt="developer" />
-                l-Haadee
+                Al-Haadee,
                 <br />
                 Full Stack Developer
             </h1>
+            <Link to="/contact" className="flat-button">CONTACT ME</Link>
         </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
