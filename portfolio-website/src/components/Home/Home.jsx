3738618eb3513402c4c5b820bfcfb178e1d14a18
Al-Haadee <alhaadee91@gmail.com>
2023-05-11 16:20:04 +0100

    adds animations to homepage

--- portfolio-website/src/components/Home/Home.jsx
+++ portfolio-website/src/components/Home/Home.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './home.css'
+import { useState, useEffect } from 'react';
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
 
 const Home = () => {
+
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = "I'm Al-Haadee".split("");
+  const firstWord = "Hi,".split("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000);
+  }, [])
+  
+
   return (
     <div className='container home-page'>
         <div className='text-zone'>
-            <h1> Hi, <br /> I'm
-                Al-Haadee
+            <h1>  <AnimatedLetters 
+                letterClass={letterClass} 
+                strArray={firstWord}
+                idx={12}/> 
+                <br />
+                <AnimatedLetters 
+                letterClass={letterClass} 
+                strArray={nameArray}
+                idx={15}/>
                 <br />
             </h1>
             <h2>Full Stack Developer</h2>
