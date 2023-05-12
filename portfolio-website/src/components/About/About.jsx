04a3ae180e9f994c0a4cb38dca9d966c982abd2a
Al-Haadee <alhaadee91@gmail.com>
2023-05-12 17:39:39 +0100

    creates about me component

---
+++ portfolio-website/src/components/About/About.jsx
@@ -0,0 +1,39 @@
+import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
+import "../Layout/Layout.css";
+import { useState, useEffect } from "react";
+
+import React from "react";
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 2500);
+  }, []);
+
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={"About me".split("")}
+            idx={12}
+          />
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+          eaque qui, laboriosam, assumenda quam quisquam officiis incidunt illum
+          laudantium earum labore similique fugit enim doloribus facere totam.
+          Quas, ducimus nemo.
+        </p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, asperiores!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, sapiente.</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ portfolio-website/src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import '../Layout/Layout.css'
+
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
