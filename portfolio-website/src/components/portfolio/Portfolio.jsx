ecfe6782cf3a2623a2472af9989539d280825114
Al-Haadee <alhaadee91@gmail.com>
2023-05-24 13:10:45 +0100

    adds my work section

--- portfolio-website/src/components/portfolio/Portfolio.jsx
+++ portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './portfolio.css'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
+import { useEffect, useState } from 'react'
 
 const Portfolio = () => {
+
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 2500);
+  }, []);
+
+
   return (
-    <div>portfolio</div>
+    <>
+    <div className="container portfolio-page">
+      <div className="text-zone-portfolio">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={"My Work".split("")}
+              idx={12}
+            />
+          </h1>
+        </div>
+      
+    </div>
+    </>
   )
 }
 
