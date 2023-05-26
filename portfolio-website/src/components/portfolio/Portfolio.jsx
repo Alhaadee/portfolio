8467ea8d4339aef856031798c6c6cbd410063272
Al-Haadee <alhaadee91@gmail.com>
2023-05-26 14:58:17 +0100

    adds more cards

--- portfolio-website/src/components/portfolio/Portfolio.jsx
+++ portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import './portfolio.css'
-import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
-import { useEffect, useState } from 'react'
+import React from "react";
+import "./portfolio.css";
+import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
+import { useEffect, useState } from "react";
 
 const Portfolio = () => {
-
   const [letterClass, setLetterClass] = useState("text-animate");
 
   useEffect(() => {
@@ -13,11 +12,10 @@ const Portfolio = () => {
     }, 2500);
   }, []);
 
-
   return (
     <>
-    <div className="container portfolio-page">
-      <div className="text-zone-portfolio">
+      <div className="container portfolio-page">
+        <div className="text-zone-portfolio">
           <h1>
             <AnimatedLetters
               letterClass={letterClass}
@@ -25,18 +23,64 @@ const Portfolio = () => {
               idx={12}
             />
           </h1>
+         <section className="projects">
+         <div className="project-card">
+            <img
+              className="project-img"
+              src="src\assets\project-screenshots\fantasyLeague_homepage.png"
+              alt="fantasy league image"
+            />
+            <div className="middle">
+              <a className="project-link" href="https://github.com/Alhaadee/fantasy_league_project">
+                <h3>Fantasy Football League</h3>
+              </a>
+            </div>
+            <p>
+              Capstone Project from Coding Bootcamp. Worked as a group in an
+              AGILE way of working to create a RESTful API with Java, Spring
+              Boot and PostgreSQL and the website using React.
+            </p>
+          </div>
+          <div className="project-card">
+            <img
+              className="project-img"
+              src="src\assets\project-screenshots\fantasyLeague_homepage.png"
+              alt="fantasy league image"
+            />
+            <div className="middle">
+              <a className="project-link" href="https://github.com/Alhaadee/fantasy_league_project">
+                <h3>Fantasy Football League</h3>
+              </a>
+            </div>
+            <p>
+              Capstone Project from Coding Bootcamp. Worked as a group in an
+              AGILE way of working to create a RESTful API with Java, Spring
+              Boot and PostgreSQL and the website using React.
+            </p>
+          </div>
+          <div className="project-card">
+            <img
+              className="project-img"
+              src="src\assets\project-screenshots\fantasyLeague_homepage.png"
+              alt="fantasy league image"
+            />
+            <div className="middle">
+              <a className="project-link" href="https://github.com/Alhaadee/fantasy_league_project">
+                <h3>Fantasy Football League</h3>
+              </a>
+            </div>
+            <p>
+              Capstone Project from Coding Bootcamp. Worked as a group in an
+              AGILE way of working to create a RESTful API with Java, Spring
+              Boot and PostgreSQL and the website using React.
+            </p>
+          </div>
+         </section>
         
-        <div className='project-card'>
-        <img className='project-img' src="src\assets\project-screenshots\fantasyLeague_homepage.png" alt="fantasy league image" />
-        <div className='middle'><h3>Fantasy Football League</h3></div>
-        <p>Lorem ipsum dolor sit amet.</p>
         </div>
-        </div>
-       
-      
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Portfolio;
