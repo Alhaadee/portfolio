1e1f07879ca0ec5a126dfab3b4ee453a6c4b2b38
Al-Haadee <alhaadee91@gmail.com>
2023-05-27 16:07:17 +0100

    adds more projects

--- portfolio-website/src/components/Layout/Layout.jsx
+++ portfolio-website/src/components/Layout/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = () => {
       <div className='page'>
         <span className='tags top-tags'>&lt;body&gt;</span>
         <Outlet/>
-        <span className='tags bottom-tags'>&lt;/body&gt;</span>
+        <span className='tags bottom-tags' id='bottom-tags'>&lt;/body&gt;</span>
       </div>
     </div>
     
--- portfolio-website/src/components/portfolio/Portfolio.jsx
+++ portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -23,7 +23,9 @@ const Portfolio = () => {
               idx={12}
             />
           </h1>
-         <section className="projects">
+          <h3>Click on each project to find out more!</h3>
+        </div>
+        <section className="projects">
          <div className="project-card">
             <img
               className="project-img"
@@ -31,7 +33,7 @@ const Portfolio = () => {
               alt="fantasy league image"
             />
             <div className="middle">
-              <a className="project-link" href="https://github.com/Alhaadee/fantasy_league_project">
+              <a className="project-link" href="https://github.com/Alhaadee/fantasy_league_project" target="_blank">
                 <h3>Fantasy Football League</h3>
               </a>
             </div>
@@ -44,40 +46,49 @@ const Portfolio = () => {
           <div className="project-card">
             <img
               className="project-img"
-              src="src\assets\project-screenshots\fantasyLeague_homepage.png"
+              src="src\assets\project-screenshots\ticTacToe_img.png"
               alt="fantasy league image"
             />
             <div className="middle">
-              <a className="project-link" href="https://github.com/Alhaadee/fantasy_league_project">
-                <h3>Fantasy Football League</h3>
+              <a className="project-link" href="https://github.com/Alhaadee/tictactoe_frontend_project" target="_blank">
+                <h3>TicTacToe</h3>
               </a>
             </div>
             <p>
-              Capstone Project from Coding Bootcamp. Worked as a group in an
-              AGILE way of working to create a RESTful API with Java, Spring
-              Boot and PostgreSQL and the website using React.
+            Used React to develop a webpage to play tic-tac-toe with API requests during the Bright Network Bootcamp. 
             </p>
           </div>
           <div className="project-card">
             <img
               className="project-img"
-              src="src\assets\project-screenshots\fantasyLeague_homepage.png"
+              src="src\assets\project-screenshots\ecommerce_drawing.png"
               alt="fantasy league image"
             />
             <div className="middle">
-              <a className="project-link" href="https://github.com/Alhaadee/fantasy_league_project">
-                <h3>Fantasy Football League</h3>
+              <a className="project-link" href="https://github.com/Alhaadee/ecommerce_project" target="_blank">
+                <h3>Ecommerce Site</h3>
               </a>
             </div>
             <p>
-              Capstone Project from Coding Bootcamp. Worked as a group in an
-              AGILE way of working to create a RESTful API with Java, Spring
-              Boot and PostgreSQL and the website using React.
+            Created RESTful API for an eCommerce site, using Spring Boot, JPA and PostgreSQL. Also used Mockito and AssertJ for unit tests. Currently working on learning React Query and Bootstrap to create the front-end.
+            </p>
+          </div>
+          <div className="project-card">
+            <img
+              className="project-img"
+              src="src\assets\project-screenshots\adventure_game_overview.png"
+              alt="fantasy league image"
+            />
+            <div className="middle">
+              <a className="project-link" href="https://github.com/Alhaadee/adventure_game_api_project" target="_blank">
+                <h3>Adventure Game API</h3>
+              </a>
+            </div>
+            <p>
+            Created RESTful API for a text-based game using Spring Boot and a PostgreSQL database during the Bright Network Bootcamp. The game was played by making various HTTP requests using Postman.
             </p>
           </div>
          </section>
-        
-        </div>
       </div>
     </>
   );
